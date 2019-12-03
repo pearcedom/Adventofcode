5413512fac4811f41de620ee4fbaef2ed39e5757
dp1 <dominic.pearce@fiosgenomics.com>
2019-12-03 09:52:50 +0000

    removed solution code from function source code

--- src/Dec2.jl
+++ src/Dec2.jl
@@ -28,20 +28,3 @@ opcode2func(code) = @match (code) begin
     1 => +
     2 => *
 end
-
-## Part 1
-#read_program("aoc-2-1.txt") |> 
-#    x->init_program(x, 12, 2) |> 
-#    process_block! |> 
-#    x->getindex(x, 1)
-#
-## Part 2
-#for i in 1:100
-#    for j in 1:100
-#        out = read_program("aoc-2-1.txt") |> 
-#            x->init_program(x, i, j) |> 
-#            process_block! |> 
-#            x->getindex(x, 1) 
-#        if out === 19690720 return i*100 + j end
-#    end
-#end
